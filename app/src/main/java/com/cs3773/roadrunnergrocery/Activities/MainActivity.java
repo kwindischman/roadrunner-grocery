7636bb64da977d0ba8c907243bacc5ae03822a57
Kai Windischman <kwindischman@gmail.com>
2019-12-04 17:42:12 -0600

    Open LoginActivity on account_icon click

--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/MainActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/MainActivity.java
@@ -2,23 +2,33 @@ package com.cs3773.roadrunnergrocery.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.cs3773.roadrunnergrocery.R;
 
-//TODO: Possibly remove the SearchActivity and ItemInfoActivity
-// to use fragments instead
 public class MainActivity extends AppCompatActivity {
 
     // Class variables here
+    private ImageView mAccountIcon;
 
     // Lifecycle methods here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupAccountIcon();
     }
 
     // Other methods here
+    private void setupAccountIcon() {
+        mAccountIcon = findViewById(R.id.account_icon);
 
+        mAccountIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
 }
