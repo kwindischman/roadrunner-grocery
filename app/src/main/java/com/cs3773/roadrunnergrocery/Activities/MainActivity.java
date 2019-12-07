082cee9b3543f168e3e658eacbd4cd734b339177
Kai Windischman <kwindischman@gmail.com>
2019-12-07 15:01:18 -0600

    Add 'See More' button to promotions in main activity

--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/MainActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/MainActivity.java
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupAccountIcon();
+        setupProducts();
+        setupPromotions();
+
+        // TODO: Set up the two Promotion views with random items
     }
 
     // Other methods here
@@ -30,5 +34,24 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void setupProducts() {
+        findViewById(R.id.product_image_1).setOnClickListener(v -> {
+            // Open Search activity and show produce
+        });
+        findViewById(R.id.product_image_2).setOnClickListener(v -> {
+            // Open Search activity and show meats
+        });
+        findViewById(R.id.product_image_3).setOnClickListener(v -> {
+            // Open Search activity and show snacks
+        });
+        findViewById(R.id.product_image_4).setOnClickListener(v -> {
+            // Open Search activity and show frozen foods
+        });
+    }
+
+    private void setupPromotions() {
+        // TODO: Grab two random promotions and use their info to make the views
     }
 }
