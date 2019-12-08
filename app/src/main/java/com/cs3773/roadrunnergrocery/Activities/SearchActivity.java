5e37f5c89ba5895821b59744f37b1ee230f18f26
Kai Windischman <kwindischman@gmail.com>
2019-12-08 15:05:15 -0600

    Change account icon to back icon in SearchActivity

--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/SearchActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/SearchActivity.java
@@ -1,6 +1,8 @@
 package com.cs3773.roadrunnergrocery.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import com.cs3773.roadrunnergrocery.Adapters.SearchAdapter;
 import com.cs3773.roadrunnergrocery.Models.Product;
@@ -25,6 +27,21 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        setupCartIcon();
+        setupRecyclerView();
+        setupSearch();
+        findViewById(R.id.back_arrow).setOnClickListener(v -> onBackPressed());
+    }
+
+    //Other methods here
+    public void setupCartIcon() {
+        findViewById(R.id.cart_icon).setOnClickListener(v -> {
+            Intent intent = new Intent(this, CartActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    public void setupRecyclerView() {
         RecyclerView recyclerView = findViewById(R.id.search_recycler);
 
         // TODO: Get list of Products related to the search term (name, tags, desc, etc)
@@ -35,6 +52,17 @@ public class SearchActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    //Other methods here
+    private void setupSearch() {
+        // TODO: Implement some sort of search system
+        // TODO: Possibly make a class that handles all of the search functionality since it shows
+        //  up throughout most of the app
+        SearchView searchView = findViewById(R.id.search);
 
+        // TODO: Change this from onClickListener to a listener that activates when user searches
+        //  for a term
+        searchView.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
+        });
+    }
 }
