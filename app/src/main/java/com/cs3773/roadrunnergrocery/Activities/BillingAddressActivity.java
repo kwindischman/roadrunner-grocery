d0e1fe2b2551fd06979a98a412aca22c6126ed79
kbo256 <kbo256@my.utsa.edu>
2019-12-09 02:53:24 -0600

    Update BillingAddressActivity.java

--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/BillingAddressActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/BillingAddressActivity.java
@@ -2,10 +2,15 @@ package com.cs3773.roadrunnergrocery.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.cs3773.roadrunnergrocery.R;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class BillingAddressActivity extends AppCompatActivity {
 
@@ -33,8 +38,87 @@ public class BillingAddressActivity extends AppCompatActivity {
 
             private void CheckBillingInfo()
             {
+                if(TextUtils.isEmpty(billingName.getText().toString()))
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter full name.";
+                    int duration = Toast.LENGTH_SHORT;
 
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                }
+
+                else if(TextUtils.isEmpty(billingPhoneNumber.getText().toString()))
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter phone number.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                else if(TextUtils.isEmpty(billingAddress.getText().toString()))
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter home address.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                else if(TextUtils.isEmpty(billingCity.getText().toString()))
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter city.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                else if(TextUtils.isEmpty(billingZipCode.getText().toString()))
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter zip code.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                else if (TextUtils.isEmpty(billingState.getText().toString()))
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter state.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                else
+                {
+                    confirmOrderWithBillingAdderess();
+                }
             }
+
+            private void confirmOrderWithBillingAdderess()
+            {
+                // Give user confirmation on order
+                Context context = getApplicationContext();
+                CharSequence text = "Order Confirmed";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                // return user to home menu
+                Intent intent = new Intent(BillingAddressActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
         });
     }
 }
