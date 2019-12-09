9be4ffd3cdcffaf4a9d98be204ada3abe318a899
kbo256 <kbo256@my.utsa.edu>
2019-12-08 19:46:47 -0600

    update checkoutActivity

--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/CheckoutActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/CheckoutActivity.java
@@ -1,6 +1,9 @@
 package com.cs3773.roadrunnergrocery.Activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cs3773.roadrunnergrocery.R;
 
@@ -10,14 +13,33 @@ import androidx.appcompat.app.AppCompatActivity;
 public class CheckoutActivity extends AppCompatActivity {
 
     // Variables here
+    private EditText nameEditText, phoneEditText, addressEditText, cityEditText;
+    private Button confirmOrderButton;
 
     // Lifecycle methods here
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        // TODO: show total if necessary
+
+        confirmOrderButton = (Button) findViewById(R.id.confirm_order_button);
+        //nameEditText = (EditText) findViewById(R.id.);
+
+        //Other methods here
+        confirmOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+
+
     }
 
-    //Other methods here
+
 
 }
