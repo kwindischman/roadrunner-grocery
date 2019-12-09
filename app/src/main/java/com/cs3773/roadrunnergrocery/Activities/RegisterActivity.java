9ce4655d35fbe9a0f494972a74148f2f729831d6
Cheyenne Sanchez <31325909+cza267@users.noreply.github.com>
2019-12-09 13:36:06 -0600

    Register v0.5.2
    Added Account Information to RegisterActivity

--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/RegisterActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/RegisterActivity.java
@@ -21,12 +21,20 @@ import com.cs3773.roadrunnergrocery.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText usernameT, passwordT;
-    private String username, password;
+    private EditText usernameT, passwordT, emailT, phoneT, streetT;
+    private EditText stateT, cityT, zipCodeT, aptNumT;
+    private String username, password, emailAdd, phoneNum, streetAdd;
+    private String state, zipCode, aptNum, cityStr;
     public static final String PREFERENCE = "preference";
     public static final String PREF_NAME = "name";
     public static final String PREF_PASS = "password";
-
+    public static final String PREF_EMAIL = "email";
+    public static final String PREF_PHONE = "phone";
+    public static final String PREF_STREET = "street";
+    public static final String PREF_APT = "aptNum";
+    public static final String PREF_CITY = "city";
+    public static final String PREF_STATE = "state";
+    public static final String PREF_ZIP = "zipCode";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +45,13 @@ public class RegisterActivity extends AppCompatActivity {
         Button registerB = findViewById(R.id.registerRButton);
         TextView loginT = findViewById(R.id.loginRText);
         TextView asGuestT = findViewById(R.id.asGuestText);
+        emailT = findViewById(R.id.emailText);
+        phoneT = findViewById(R.id.phoneNumber);
+        streetT = findViewById(R.id.streetAddress);
+        aptNumT = findViewById(R.id.aptNum);
+        stateT = findViewById(R.id.state);
+        zipCodeT = findViewById(R.id.zipCode);
+        cityT = findViewById(R.id.cityName);
 
         registerB.setOnClickListener(view -> {
             if(validUserData()){
@@ -45,6 +60,13 @@ public class RegisterActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString(PREF_NAME,username);
                 editor.putString(PREF_PASS,password);
+                editor.putString(PREF_EMAIL,emailAdd);
+                editor.putString(PREF_PHONE,phoneNum);
+                editor.putString(PREF_STREET,streetAdd);
+                editor.putString(PREF_STATE,state);
+                editor.putString(PREF_APT,aptNum);
+                editor.putString(PREF_CITY,cityStr);
+                editor.putString(PREF_ZIP,zipCode);
                 editor.apply();
                 finish();
             }
@@ -65,6 +87,13 @@ public class RegisterActivity extends AppCompatActivity {
     private boolean validUserData() {
         username = usernameT.getText().toString().trim();
         password = passwordT.getText().toString().trim();
+        emailAdd = emailT.getText().toString().trim();
+        phoneNum = phoneT.getText().toString().trim();
+        streetAdd = streetT.getText().toString().trim();
+        aptNum = aptNumT.getText().toString().trim();
+        state = stateT.getText().toString().trim();
+        zipCode = zipCodeT.getText().toString().trim();
+        cityStr =cityT.getText().toString().trim();
         return !(username.isEmpty() || password.isEmpty());
     }
 }
