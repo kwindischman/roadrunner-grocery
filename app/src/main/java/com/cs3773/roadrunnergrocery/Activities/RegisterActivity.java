821cc7875679741b95ecc687d837ef7a39bf5063
Cheyenne Sanchez <31325909+cza267@users.noreply.github.com>
2019-12-10 13:34:50 -0600

    revert Mistake Commit v0.2
    reverting back to login and register

--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/RegisterActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/RegisterActivity.java
@@ -18,13 +18,19 @@ import com.cs3773.roadrunnergrocery.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+<<<<<<< HEAD
     private EditText usernameT, passwordT, emailT, cardNumT, streetT;
     private EditText stateT, cityT, zipCodeT, aptNumT;
     private String username, password, emailAdd, cardNum, streetAdd;
     private String state, zipCode, aptNum, cityStr;
+=======
+    private EditText usernameT, passwordT;
+    private String username, password;
+>>>>>>> parent of 9ce4655... Register v0.5.2
     public static final String PREFERENCE = "preference";
     public static final String PREF_USER = "name";
     public static final String PREF_PASS = "password";
+<<<<<<< HEAD
     public static final String PREF_EMAIL = "email";
     public static final String PREF_CARD= "card";
     public static final String PREF_STREET = "street";
@@ -32,6 +38,9 @@ public class RegisterActivity extends AppCompatActivity {
     public static final String PREF_CITY = "city";
     public static final String PREF_STATE = "state";
     public static final String PREF_ZIP = "zipCode";
+=======
+
+>>>>>>> parent of 9ce4655... Register v0.5.2
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +51,7 @@ public class RegisterActivity extends AppCompatActivity {
         Button registerB = findViewById(R.id.registerRButton);
         TextView loginT = findViewById(R.id.loginRText);
         TextView asGuestT = findViewById(R.id.asGuestText);
+<<<<<<< HEAD
         emailT = findViewById(R.id.emailText);
         cardNumT = findViewById(R.id.cardNumber);
         streetT = findViewById(R.id.streetAddress);
@@ -49,6 +59,8 @@ public class RegisterActivity extends AppCompatActivity {
         stateT = findViewById(R.id.state);
         zipCodeT = findViewById(R.id.zipCode);
         cityT = findViewById(R.id.cityName);
+=======
+>>>>>>> parent of 9ce4655... Register v0.5.2
 
         registerB.setOnClickListener(view -> {
             if(validUserData()){
@@ -57,6 +69,7 @@ public class RegisterActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString(PREF_USER,username);
                 editor.putString(PREF_PASS,password);
+<<<<<<< HEAD
                 editor.putString(PREF_EMAIL,emailAdd);
                 editor.putString(PREF_CARD,cardNum);
                 editor.putString(PREF_STREET,streetAdd);
@@ -64,6 +77,8 @@ public class RegisterActivity extends AppCompatActivity {
                 editor.putString(PREF_APT,aptNum);
                 editor.putString(PREF_CITY,cityStr);
                 editor.putString(PREF_ZIP,zipCode);
+=======
+>>>>>>> parent of 9ce4655... Register v0.5.2
                 editor.apply();
                 finish();
             }
@@ -83,6 +98,7 @@ public class RegisterActivity extends AppCompatActivity {
     private boolean validUserData() {
         username = usernameT.getText().toString().trim();
         password = passwordT.getText().toString().trim();
+<<<<<<< HEAD
         emailAdd = emailT.getText().toString().trim();
         cardNum = cardNumT.getText().toString().trim();
         streetAdd = streetT.getText().toString().trim();
@@ -90,6 +106,8 @@ public class RegisterActivity extends AppCompatActivity {
         state = stateT.getText().toString().trim();
         zipCode = zipCodeT.getText().toString().trim();
         cityStr =cityT.getText().toString().trim();
+=======
+>>>>>>> parent of 9ce4655... Register v0.5.2
         return !(username.isEmpty() || password.isEmpty());
     }
 }
