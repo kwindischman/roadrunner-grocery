4b3a18a18c5708b8c4e30e5e49acccbab4819df1
Marcus Moffett <moffett.md@gmail.com>
2019-12-10 20:37:42 -0600

    PromotionActivity w/ Holidays CheckoutActivity recovered

--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/CheckoutActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/CheckoutActivity.java
@@ -1,11 +1,12 @@
 package com.cs3773.roadrunnergrocery.Activities;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
-import com.cs3773.roadrunnergrocery.R;
+        import com.cs3773.roadrunnergrocery.Adapters.CartAdapter;
+        import com.cs3773.roadrunnergrocery.R;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.annotation.Nullable;
+        import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -19,5 +20,83 @@ public class CheckoutActivity extends AppCompatActivity {
     }
 
     //Other methods here
+    /*
+        TODO:
+            : Get cart information
+            : ~ Paypal API registration
+            :
+            : Set error codes for Paypal
+            : //Paypal Implementation Follows//
+            : String command = " --- plaintext command to Paypal --- ";
+            : ProcessBuilder builder = new ProcessBuilder(command.split(" "));
+            : //End Paypal Implementation//
+            : Configure API / Calls
+            :
+            : Local Checkout tax rate calculation by location
+
+     */
+/* For a future version
+    protected int paypalCheckout(CartAdapter customerOrder)
+    {
+        //Get cart information
+        String command = "curl -v -X GET https://api.sandbox.paypal.com/v1/payment-experience/web-profiles/XP-8YTH-NNP3-WSVN-3C76 \\\n" +
+                "  -H \"Content-Type: application/json\" \\\n" +
+                "  -H \"Authorization: Bearer AXqi6SV39YYkGRelXU2mqFfOFvSsHhWQS1mWxlnG1o2wXM8rbKlsvmuOvI7qozwsW1SjBz0CR0Cs15LX\"";
+        ProcessBuilder builder = new ProcessBuilder(command.split(" "));
+
+        return 0;
+    }
+*/
+    protected double localCheckout(CartAdapter customerOrder)
+    {
+        float cartTotal = 0.00F;
+        for (int i = 0; i < customerOrder.getItemCount() ; i++)
+        {
+            customerOrder.mProductPairList.get(i);
+        }
+        float taxRate = 1.0F;
+        float discountPercent = 0F;
+        float discountMonetary = 0.0F;
+        float deliveryCost = 0.00F;
+
+        discountMonetary = PromotionActivity.couponMonetary();
+        discountPercent = PromotionActivity.couponPercent();
+
+        //Discount Calculations
+        //Find available discounts (user data?)
+
+        if((discountMonetary > 0) || (discountPercent > 0))
+        {
+            double intermediateTotal = -9999; //debug value: -9999
+            if(discountMonetary > 0)
+            {
+                intermediateTotal = cartTotal - discountMonetary;
+            }
+
+            if(discountPercent > 0 && intermediateTotal > 0)
+            {
+                if(intermediateTotal < (cartTotal - (cartTotal * discountPercent)))
+                {
+                    intermediateTotal = cartTotal - (cartTotal * discountPercent);
+                    intermediateTotal = Math.round(intermediateTotal * 100.0) / 100.0; //Cents accuracy
+                }
+            }else
+            {
+                intermediateTotal = (cartTotal - (cartTotal * discountPercent));
+                intermediateTotal = Math.round(intermediateTotal * 100.0) / 100.0; //Cents accuracy
+            }
+        }
+
+
+        //Delivery & Tax Calculations
+        //Get cart information
+        //Get & parse user address
+        //Lookup tax rate according to location
+
+        //Final Cost
+        double finalTotal = cartTotal + (cartTotal * taxRate) + deliveryCost;
+
+        return finalTotal;
+    }
 
 }
--- app/src/main/java/com/cs3773/roadrunnergrocery/Activities/PromotionActivity.java
+++ app/src/main/java/com/cs3773/roadrunnergrocery/Activities/PromotionActivity.java
@@ -7,9 +7,18 @@ import com.cs3773.roadrunnergrocery.R;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
 public class PromotionActivity extends AppCompatActivity {
 
-    // Variables here
+    public static final float NEW_YEAR_SALE = .10F;
+    public static final float MOTHERS_DAY_SALE = .10F;
+    public static final float FATHERS_DAY_SALE = .10F;
+    public static final float VALENTINES_SALE = .14F;
+    public static final float INDPENDENCE_DAY_SALE = .17F;
+    public static final float HALLOWEEN_SALE = .13F;
+    public static final float BLACK_FRIDAY_SALE = .4F;
+    public static final float CHRISTMAS_SALE = .25F;
+
 
     // Lifecycle methods here
     @Override
@@ -18,6 +27,66 @@ public class PromotionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_promotion);
     }
 
-    //Other methods here
+    public static float couponPercent()
+    {
+        float fDiscount = 0.0f;
+        Calendar today;
+        today = Calendar.getInstance();
+        int tDay, tMonth;
+        tDay = today.get(Calendar.DAY_OF_MONTH);
+        tMonth = today.get(Calendar.MONTH);
 
+
+        switch(tMonth) //Holiday Discounts
+        {
+            case 1:
+                if(tDay == 1)
+                    return NEW_YEAR_SALE;
+                break;
+            case 2:
+                if(tDay == 14)
+                    return VALENTINES_SALE;
+                break;
+            case 5:
+                if(tDay == 12)
+                    return MOTHERS_DAY_SALE;
+                break;
+            case 6:
+                if(tDay == 16)
+                    return FATHERS_DAY_SALE;
+                break;
+            case 7:
+                if(tDay == 4)
+                    return INDPENDENCE_DAY_SALE;
+                break;
+            case 10:
+                if(tDay == 31)
+                    return HALLOWEEN_SALE;
+               break;
+            case 11:
+                if((today.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) &&
+                        (tDay > 22) && (tDay < 30))
+                    return BLACK_FRIDAY_SALE;
+                break;
+            case 12:
+                if((tDay > 18) && (tDay < 24))
+                    return CHRISTMAS_SALE;
+                break;
+            default: break;
+        }
+
+        if((tDay == 13) && (today.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY))
+            fDiscount = HALLOWEEN_SALE;
+
+        return fDiscount;
+    }
+
+
+    public static float couponMonetary()
+    {
+        float fDiscount = 0.0f;
+
+
+        return fDiscount;
+    }
 }
